e7af7c6b44bd2a1374b4784a6a1e746dfc724479
lukanosudre <lukanosudre@gmail.com>
2022-07-11 16:23:36 -0300

    Requisito 9, mas precisa de detalhes

--- src/components/ProductDetailed.js
+++ src/components/ProductDetailed.js
@@ -16,22 +16,48 @@ class ProductDetailed extends React.Component {
     this.fetchProduct(id);
   }
 
+  handleAddProductToCart(product) {
+    const items = this.getCartItems();
+    if (items) {
+      const newItemsList = [...items, product];
+      localStorage.setItem('carrinho', JSON.stringify(newItemsList));
+    } else {
+      localStorage.setItem('carrinho', JSON.stringify([product]));
+    }
+  }
+
+  getCartItems() {
+    const items = JSON.parse(localStorage.getItem('carrinho'));
+    return items;
+  }
+
   fetchProduct = async (id) => {
     const product = await getProductInfo(id);
     this.setState({
       productDetails: product,
     });
+    console.log(product);
   }
 
   render() {
+    const { match: { params: { id } } } = this.props;
     const { productDetails } = this.state;
+    const { price, thumbnail, title } = productDetails;
+
     return (
       <div>
-        <Link to="/shoppingcart">carrinho</Link>
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">carrinho</Link>
         <div className="product-detailed">
-          <span data-testid="product-detail-name">{ productDetails.title }</span>
-          <img alt={ productDetails.title } src={ productDetails.thumbnail } />
-          <span>{ productDetails.price }</span>
+          <span data-testid="product-detail-name">{title}</span>
+          <img alt={ title } src={ thumbnail } />
+          <span>{productDetails.price}</span>
+          <button
+            data-testid="product-detail-add-to-cart"
+            type="button"
+            onClick={ () => this.handleAddProductToCart({ id, price, thumbnail, title }) }
+          >
+            Adicionar ao carrinho
+          </button>
         </div>
       </div>
     );
